316eb956c0df595133e403535a047c509a830352
Xavian Moody <XavianMoody1025@gmail.com>
2024-01-26 18:44:07 -0500

    Finished work section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,11 +18,38 @@ function toggleMobileMenu() {
   });
 }
 
-function sectionTransition() {
+function heroSectionTransition() {
   const arrow = document.querySelector(".down-arrow");
-  const section = document.querySelector(".work");
+  const nextSection = document.querySelector(".work");
+  const currentSection = document.querySelector(".hero");
   const introduction = document.querySelector(".introduction");
   const summary = document.querySelector(".summary");
+
+  window.addEventListener("scroll", () => {
+    let options = { threshold: 0.3 };
+
+    function callback(entries) {
+      [...entries].forEach((entry) => {
+        if (entry.isIntersecting) {
+          introduction.style.animationDelay = "0.5s";
+          introduction.classList.add("drop-down-enter");
+          introduction.classList.remove("drop-down-exit");
+
+          summary.style.animationDelay = "0.5s";
+          summary.classList.add("drop-down-enter");
+          summary.classList.remove("drop-down-exit");
+
+          arrow.style.animationDelay = "0.5s";
+          arrow.classList.add("drop-down-enter");
+          arrow.classList.remove("drop-down-exit");
+        }
+      });
+    }
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(currentSection);
+  });
 
   arrow.addEventListener("click", () => {
     introduction.style.animationDelay = "0s";
@@ -38,10 +65,31 @@ function sectionTransition() {
     arrow.classList.add("drop-down-exit");
 
     setTimeout(() => {
-      section.scrollIntoView();
+      nextSection.scrollIntoView();
     }, 1500);
   });
 }
 
-sectionTransition();
+function workSectionTransition() {
+  const currentSection = document.querySelector(".work");
+
+  window.addEventListener("scroll", () => {
+    let options = { threshold: 0.3 };
+
+    function callback(entries) {
+      [...entries].forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("drop-down-enter");
+        }
+      });
+    }
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(currentSection);
+  });
+}
+
+heroSectionTransition();
+workSectionTransition();
 toggleMobileMenu();
